80a6c9cf37c0c2e3e27dad3877bf4a9bef4314d6
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-11-17 16:15:23 +0900

    クソが
    画像追加。
    画像切り替え。

--- TeamProject/TeamProject/BlindGirl.cpp
+++ TeamProject/TeamProject/BlindGirl.cpp
@@ -4,11 +4,16 @@
 
 Girl::Girl()
 {
+	m_wait = 0;
+	m_walk = 0;
+	m_jamp = 0;
 	m_timer = 0.0f;	
 	m_girlposx = 100.0f;
 	m_girlposy = 450.0f;
 	m_speed = 4.0f;
 	m_gravity = 0.0f;
+	m_waiting = false;
+	m_walking = false;
 	m_floating  = false;
 }
 
@@ -21,6 +26,7 @@ void Girl::Move()
 	if (CheckHitKey(KEY_INPUT_LEFT) && m_girlposx >= 15.0f)
 	{
 		m_girlposx -= m_speed;
+		m_walking = true;
 
 		if (CheckHitKey(KEY_INPUT_UP) && m_girlposy >= 450.0f)
 		{
@@ -31,6 +37,7 @@ void Girl::Move()
 	else if (CheckHitKey(KEY_INPUT_RIGHT) && m_girlposx <= 625.0f)
 	{
 		m_girlposx += m_speed;
+		m_walking = true;
 
 		if (CheckHitKey(KEY_INPUT_UP) && m_girlposy >= 450.0f)
 		{
@@ -43,6 +50,7 @@ void Girl::Move()
 		m_gravity = -5.0f;
 		m_floating = true;
 	}
+
 	if (m_floating == true)
 	{
 		m_girlposy += m_gravity;
@@ -52,11 +60,29 @@ void Girl::Move()
 			m_floating = false;
 			m_gravity = 0.0f;
 		}
-	}	
+	}
+	else
+	{
+		m_waiting = true;
+	}
 }
 
 void Girl::Draw()
 {
+	m_wait = LoadGraph("Res/Girl/taikiB_R.png");
 	m_walk = LoadGraph("Res/Girl/girl_walk1.png");
-	DrawRotaGraph(m_girlposx, m_girlposy, 0.3, 0.0, m_walk, TRUE);
+	m_jamp = LoadGraph("Res/Girl/junp_R_B.png");
+
+	if (m_floating == true)
+	{
+		DrawRotaGraph(m_girlposx, m_girlposy, 0.3, 0.0, m_jamp, TRUE);
+	}
+	else if (m_walking == true)
+	{
+		DrawRotaGraph(m_girlposx, m_girlposy, 0.3, 0.0, m_walk, TRUE);
+	}
+	else if(m_waiting == true)
+	{
+		DrawRotaGraph(m_girlposx, m_girlposy, 0.3, 0.0, m_wait, TRUE);
+	}
 }
--- TeamProject/TeamProject/BlindGirl.h
+++ TeamProject/TeamProject/BlindGirl.h
@@ -12,12 +12,16 @@ public:
 	void Draw();
 
 private:
+	int m_wait;
 	int m_walk;
+	int m_jamp;
 	int m_timer;
 	float m_girlposx;
 	float m_girlposy;
 	float m_speed;
 	float m_gravity;
+	bool m_waiting;
+	bool m_walking;
 	bool m_floating;
 };
 
