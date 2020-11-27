2e37d3f9de906921b3f68b0cabce3c143f8e6f4c
saitou-hiroaki <3110hiroai0410@gmail.com>
2020-11-27 11:17:40 +0900

    めちゃめちゃ回る橋

--- TeamProject/TeamProject/Processing.cpp
+++ TeamProject/TeamProject/Processing.cpp
@@ -3,8 +3,10 @@
 #include "Map.h"
 #include "Player.h"
 #include "Enemy.h"
+#include "Bridge.h"
 
 Player player;
+Bridge bridge;
 
 Processing::Processing()
 {
@@ -17,9 +19,11 @@ Processing::~Processing()
 void Processing::Game()
 {
 	player.Move();
+	bridge.Move();
 }
 
 void Processing::Draw()
 {
 	player.Draw();
+	bridge.Draw();
 }
